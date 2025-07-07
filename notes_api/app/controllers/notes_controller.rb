4751f07b5acee009bcf5701cc5da5b010023f424
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-08 10:04:26 +1200

    feat(notes_api): add NotesController with index, show, create JSON actions

---
+++ notes_api/app/controllers/notes_controller.rb
@@ -0,0 +1,27 @@
+class NotesController < ApplicationController
+  def index
+    render json: Note.all
+  end
+
+  def show
+    note = Note.find(params[:id])
+
+    render json: note
+  end
+
+  def create
+    note = Note.new(note_params)
+
+    if note.save
+      render json: note, status: :created
+    else
+      render json: { errors: note.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
+end
