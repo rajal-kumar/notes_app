c7a7d9f6ba47a2904c70bd34e3291add8703ca2d
Rajal Kumar <rajalkumar@protonmail.com>
2025-07-08 10:19:33 +1200

    test(notes_api): add Note model specs for validations

--- notes_api/spec/models/note_spec.rb
+++ notes_api/spec/models/note_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when posting a note" do
+    it "is valid with a title and body" do
+      note = Note.new(title: "Test", body: "Some context")
+      expect(note).to be_valid
+    end
+
+    it "is invalid without a title" do
+      note = Note.new(title: nil, body: "Some context")
+
+      expect(note).to_not be_valid
+      expect(note.errors[:title]).to include("can't be blank")
+    end
+
+    it "is invalid without a body" do
+      note = Note.new(title: "Test", body: nil)
+
+      expect(note).to_not be_valid
+      expect(note.errors[:body]).to include("can't be blank")
+    end
+  end
 end
